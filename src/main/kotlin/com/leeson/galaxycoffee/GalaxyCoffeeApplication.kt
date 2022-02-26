54793bb57a771b290dbee2fae62e2922156bfcec
jaychy-yy <0816jinjin@gmail.com>
2022-02-26 23:53:44 +0900

    chore: 프로젝트 설정

---
+++ src/main/kotlin/com/leeson/galaxycoffee/GalaxyCoffeeApplication.kt
@@ -0,0 +1,11 @@
+package com.leeson.galaxycoffee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GalaxyCoffeeApplication
+
+fun main(args: Array<String>) {
+    runApplication<GalaxyCoffeeApplication>(*args)
+}
---
+++ src/test/kotlin/com/leeson/galaxycoffee/GalaxyCoffeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leeson.galaxycoffee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GalaxyCoffeeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
